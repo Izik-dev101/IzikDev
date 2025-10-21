38888a8724b23e82c6ebec71a35e328a02445b90
Izik Charles <zikoraikeogu5@gmail.com>
2025-10-21 12:21:18 +0100

    Fix router structure & loading layout

---
+++ src/components/Loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black text-white text-xl animate-pulse">
+      🚀 Loading Izik.codes…
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import App from './App'
 import './index.css'
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
